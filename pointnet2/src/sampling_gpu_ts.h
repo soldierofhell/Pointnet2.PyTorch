cfe3a18eac8fb1c8f7e43e36782bc72a99a13f45
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2019-09-23 16:37:04 +0200

    Update sampling_gpu_ts.h

--- pointnet2/src/sampling_gpu_ts.h
+++ pointnet2/src/sampling_gpu_ts.h
@@ -7,6 +7,6 @@
 
 
 void furthest_point_sampling_kernel_launcher(int64_t b, int64_t n, int64_t m, 
-    const double *dataset, double *temp, int_64 *idxs, cudaStream_t stream);
+    const double *dataset, double *temp, int64_t *idxs, cudaStream_t stream);
 
 #endif
