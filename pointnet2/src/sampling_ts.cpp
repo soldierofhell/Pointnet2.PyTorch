321137d947aa1d50fa4227c27308d8cc0177ff7f
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2019-09-23 16:18:52 +0200

    Update sampling_ts.cpp

--- pointnet2/src/sampling_ts.cpp
+++ pointnet2/src/sampling_ts.cpp
@@ -4,7 +4,7 @@
 #include <THC/THC.h>
 #include <torch/script.h>
 
-#include "sampling_gpu.h"
+#include "sampling_gpu_ts.h"
 
 
 extern THCState *state;
@@ -13,37 +13,12 @@ static auto registry =
   torch::RegisterOperators("pointnet2_ts::furthest_point_sampling_wrapper", &furthest_point_sampling_wrapper);
 
 
-int gather_points_wrapper_fast(int b, int c, int n, int npoints, 
-    at::Tensor points_tensor, at::Tensor idx_tensor, at::Tensor out_tensor){
-    const float *points = points_tensor.data<float>();
-    const int *idx = idx_tensor.data<int>();
-    float *out = out_tensor.data<float>();
-
-    cudaStream_t stream = THCState_getCurrentStream(state);
-    gather_points_kernel_launcher_fast(b, c, n, npoints, points, idx, out, stream);
-    return 1;
-}
-
-
-int gather_points_grad_wrapper_fast(int b, int c, int n, int npoints, 
-    at::Tensor grad_out_tensor, at::Tensor idx_tensor, at::Tensor grad_points_tensor) {
-
-    const float *grad_out = grad_out_tensor.data<float>();
-    const int *idx = idx_tensor.data<int>();
-    float *grad_points = grad_points_tensor.data<float>();
-
-    cudaStream_t stream = THCState_getCurrentStream(state);
-    gather_points_grad_kernel_launcher_fast(b, c, n, npoints, grad_out, idx, grad_points, stream);
-    return 1;
-}
-
-
-int furthest_point_sampling_wrapper(int b, int n, int m, 
+int furthest_point_sampling_wrapper(int_64 b, int_64 n, int_64 m, 
     at::Tensor points_tensor, at::Tensor temp_tensor, at::Tensor idx_tensor) {
 
-    const float *points = points_tensor.data<float>();
-    float *temp = temp_tensor.data<float>();
-    int *idx = idx_tensor.data<int>();
+    const double *points = points_tensor.data<double>();
+    double *temp = temp_tensor.data<double>();
+    int_64 *idx = idx_tensor.data<int_64>();
 
     cudaStream_t stream = THCState_getCurrentStream(state);
     furthest_point_sampling_kernel_launcher(b, n, m, points, temp, idx, stream);
