1f8c3f0a365305841a54738e9737fb9b8fe775c0
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2019-09-23 16:36:04 +0200

    Update sampling_ts.cpp

--- pointnet2/src/sampling_ts.cpp
+++ pointnet2/src/sampling_ts.cpp
@@ -13,12 +13,12 @@ static auto registry =
   torch::RegisterOperators("pointnet2_ts::furthest_point_sampling_wrapper", &furthest_point_sampling_wrapper);
 
 
-int furthest_point_sampling_wrapper(int_64 b, int_64 n, int_64 m, 
+int furthest_point_sampling_wrapper(int64_t b, int64_t n, int64_t m, 
     at::Tensor points_tensor, at::Tensor temp_tensor, at::Tensor idx_tensor) {
 
     const double *points = points_tensor.data<double>();
     double *temp = temp_tensor.data<double>();
-    int_64 *idx = idx_tensor.data<int_64>();
+    int64_t *idx = idx_tensor.data<int64_t>();
 
     cudaStream_t stream = THCState_getCurrentStream(state);
     furthest_point_sampling_kernel_launcher(b, n, m, points, temp, idx, stream);
